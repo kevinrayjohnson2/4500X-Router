b33a9ecc3f6857b6c1a7107763d5fc2f713717c7
Kevin Johnson <kevin.ray.johnson2@gmail.com>
2019-11-28 22:20:37 -0600

    updated vlan config to write to file.

--- getConfig.py
+++ getConfig.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python3
 
-
 #Build out building vlans.
 
 
 class BuildingRouting:
-    def __init__(self, buildingdes, buildingvlanid, buildingvlanip):
+    def __init__(self, buildingdes, buildingvlanid, buildingvlanip, buildingsubnet,buildinggateway):
         self.buildingdes = buildingdes
         self.buildingvlanid = buildingvlanid
         self.buildingvlanip = buildingvlanip
+        self.buildingsubnet = buildingsubnet
+        self.buildinggateway = buildinggateway
 
     def buildingroutingvlan(self):
-        print('My building is ' + self.buildingdes + ' My vlan is:  ' + self.buildingvlanid + ' My building IP is: ' + self.buildingvlanip)
+         with open("mycommands.txt", "w") as f:
+            f.write('interface Vlan ' + self.buildingvlanid + '\n')
+            f.write(' description ' + self.buildingdes + '\n')
+            f.write(' vrf forwarding XXXX\n')
+            f.write(' ip address ' + self.buildingvlanip + ' ' + self.buildingsubnet + '\n')
+            f.write(' ip helper-address 4.4.4.4\n')
+            f.write(' ip helper-address 8.8.8.8\n')
+            f.write(' no ip redirects\n')
+            f.write(' ip pim passive\n')
+            f.write(' standby version 2\n')
+            f.write(' standby ' + self.buildingvlanid + ' ip ' + self.Housebuildinggateway + '\n')
+            f.write(' standby ' + self.buildingvlanid + ' priority 250\n')
+            f.write(' standby ' + self.buildingvlanid + ' preempt\n')
+            f.write(' standby ' + self.buildingvlanid + ' authentication md5 key-string nice-auth\n')
+            f.write(' ip ospf xxxx area 1.2.3.4\n')
+            f.write(' shutdowm\n')
+            f.close()
 
     @classmethod
     def from_input(cls):
         return cls(
             buildingdes = input('Enter Building Description: '),
             buildingvlanid = input('Enter Building VLAN: '),
-            buildingvlanip = input('Please enter the building vlan IP:')
+            buildingvlanip = input('Please enter the building vlan IP:'),
+            buildingsubnet = input('Please enter the building subnet:'),
+            buildinggateway = input('Please enter the building gateway:')
         )
+
+
+
+
 
 # class Buildingvoip:
 #     def __init__(self, buildingvlandes, vlanid):
@@ -46,3 +69,8 @@ class Camerarouting:
 buildingroute = BuildingRouting.from_input()
 
 BuildingRouting.buildingroutingvlan(buildingroute)
+
+
+
+
+
