cc8813a47e7d945beb586298c39f39ff6df0728c
Kevin Johnson <kevin.ray.johnson2@gmail.com>
2019-12-05 14:56:42 -0600

    WORKING COPY OF BASE

--- getConfig.py
+++ getConfig.py
@@ -8,8 +8,10 @@ root = Tk()
 root.title("Vlan Config Helper")
 content = ttk.Frame(root, padding=(10,10,120,120))
 
+# Set all Entry Vars as empty strings
+
 buildingsubnet =''
-buildingdes =''
+buildingdes = ''
 buildingvlanip =''
 buildingvlanid =''
 buildinggateway =''
@@ -17,35 +19,76 @@ buildinggateway =''
 
 # Build out the form
 bvldeslbl = ttk.Label(content, text="Enter Building Vlan Description: ")
-bdes = ttk.Entry(content, textvariable=buildingdes)
-#buildingdes = bdes.get()
+bdes = ttk.Entry(content)
 
 bvlidlbl = ttk.Label(content, text="Enter Building Vlan Id: ")
-bvlanid = ttk.Entry(content, textvariable=buildingvlanid)
+bvlanid = ttk.Entry(content)
 #buildingvlanid = bvlanid.get()
 
 bvliplbl = ttk.Label(content, text="Enter Building Vlan IP: ")
-bvlanip = ttk.Entry(content, textvariable=buildingvlanip)
+bvlanip = ttk.Entry(content)
 #buildingvlanip = bvlanip.get()
 
 bvlsnlbl = ttk.Label(content, text="Enter Building Vlan subnet: ")
-bsubnet = ttk.Entry(content, textvariable=buildingsubnet)
+bsubnet = ttk.Entry(content)
 #buildingsubnet = bsubnet.get()
 
 bvlgwlbl = ttk.Label(content, text="Enter Building Vlan gateway: ")
-bgateway = ttk.Entry(content, textvariable=buildinggateway)
+bgateway = ttk.Entry(content)
 #buildinggateway = bgateway.get()
 
-
+def clear(): 
+    #Clear the text boxes
+    bdes.delete(0, END) 
+    bvlanid.delete(0, END) 
+    bvlanip.delete(0, END)
+    bsubnet.delete(0, END) 
+    bgateway.delete(0, END) 
 
 
 def closeWindow():
     exit()
 
-BuildingRouting.from_input = ['buildingsubnet', 'buildingdes', 'buildingvlanip', 'buildingvlanid', 'buildinggateway']
 
 
-submit = ttk.Button(content, text="Submit", command=BuildingRouting.from_input())
+
+
+def from_input():
+    if (bdes.get() == "" and
+        bvlanid.get() == "" and 
+        bvlanip.get() == "" and
+        bsubnet.get() == "" and
+        bgateway.get() == ""):
+
+        print("Enter all fields")
+    else:
+
+        print("the entry is: " + bdes.get())
+      
+
+        with open("mycommands.txt", "w") as f:
+            f.write('interface Vlan ' + bdes.get() + '\n')
+            f.write(' description ' + bvlanid.get() + '\n')
+            f.write(' vrf forwarding XXXX\n')
+            f.write(' ip address ' + bvlanip.get() + ' ' + bsubnet.get() + '\n')
+            f.write(' ip helper-address 4.4.4.4\n')
+            f.write(' ip helper-address 8.8.8.8\n')
+            f.write(' no ip redirects\n')
+            f.write(' ip pim passive\n')
+            f.write(' standby version 2\n')
+            f.write(' standby ' + bvlanid.get() + ' ip ' + bgateway.get() + '\n')
+            f.write(' standby ' + bvlanid.get() + ' priority 250\n')
+            f.write(' standby ' + bvlanid.get() + ' preempt\n')
+            f.write(' standby ' + bvlanid.get() + ' authentication md5 key-string nice-auth\n')
+            f.write(' ip ospf xxxx area 1.2.3.4\n')
+            f.write(' shutdowm\n')
+            f.close()
+    
+
+
+
+
+submit = ttk.Button(content, text="Submit", command=from_input)
 
 #Cancel Button Works ----- dont invoke the function...
 cancel =ttk.Button(content, text="Cancel", command=closeWindow)
@@ -84,78 +127,12 @@ content.columnconfigure(5, weight=3)
 content.columnconfigure(6, weight=3)
 content.rowconfigure(1, weight=1)
 
-#Build out building vlans.
-
-
-class BuildingRouting:
-    def __init__(self, buildingdes, buildingvlanid, buildingvlanip, buildingsubnet,buildinggateway):
-        self.buildingdes = buildingdes
-        self.buildingvlanid = buildingvlanid
-        self.buildingvlanip = buildingvlanip
-        self.buildingsubnet = buildingsubnet
-        self.buildinggateway = buildinggateway
-
-    def buildingroutingvlan(self):
-         with open("mycommands.txt", "w") as f:
-            f.write('interface Vlan ' + self.buildingvlanid + '\n')
-            f.write(' description ' + self.buildingdes + '\n')
-            f.write(' vrf forwarding XXXX\n')
-            f.write(' ip address ' + self.buildingvlanip + ' ' + self.buildingsubnet + '\n')
-            f.write(' ip helper-address 4.4.4.4\n')
-            f.write(' ip helper-address 8.8.8.8\n')
-            f.write(' no ip redirects\n')
-            f.write(' ip pim passive\n')
-            f.write(' standby version 2\n')
-            f.write(' standby ' + self.buildingvlanid + ' ip ' + self.buildinggateway + '\n')
-            f.write(' standby ' + self.buildingvlanid + ' priority 250\n')
-            f.write(' standby ' + self.buildingvlanid + ' preempt\n')
-            f.write(' standby ' + self.buildingvlanid + ' authentication md5 key-string nice-auth\n')
-            f.write(' ip ospf xxxx area 1.2.3.4\n')
-            f.write(' shutdowm\n')
-            f.close()
-
-    @classmethod
-    def from_input(cls):
-        return cls(
-            buildingdes = bdes.get(),
-            buildingvlanid = bvlanid.get(),
-            buildingvlanip = bvlanip.get(),
-            buildingsubnet = bsubnet.get(),
-            buildinggateway = bgateway.get()
-       )
-
-
-# class Buildingvoip:
-#     def __init__(self, buildingvlandes, vlanid):
-#         self.buildingname = buildingnameBlue
-#         self.vlanid = vlanid
-
-#     def myfunc(self):
-#         print('My building is ' + self.name + 'my vlan is ' + self.vlanid) 
-
-
-
-#Build out OSPF Routing
-
-class TN2routing:
-    pass
-
-class VoIProuting:
-    pass
-
-class Camerarouting:
-    pass
-
-
-
-
-print =(BuildingRouting.buildingroutingvlan())
-
-
-
-
 
 root.mainloop()
+
+
+
+
 
 
 
